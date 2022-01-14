a64f10343f20242990cbb0ef423ab2b4ffa6d8da
TFache <thom.fache@orange.fr>
2022-01-14 16:46:01 +0100

    feat: unit test divisible_par_3_et_par_5

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -6,4 +6,7 @@ test('divisible_par_3_seulement', () => {
 test('divisible_par_5_seulement', () => {
     expect(fizzBuzz(5)).toBe("Buzz")
 })
+test('divisible_par_3_et_par_5', () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz")
+})
 
