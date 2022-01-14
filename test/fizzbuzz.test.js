e7a9064274a50354972e69547cf75d16ac071c59
TFache <thom.fache@orange.fr>
2022-01-14 16:58:03 +0100

    feat: changed test values for each unit test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,15 +1,15 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
 test('divisible_par_3_seulement', () => {
-    expect(fizzBuzz(3)).toBe("Fizz")
+    expect(fizzBuzz(63)).toBe("Fizz")
 })
 test('divisible_par_5_seulement', () => {
-    expect(fizzBuzz(5)).toBe("Buzz")
+    expect(fizzBuzz(125)).toBe("Buzz")
 })
 test('divisible_par_3_et_par_5', () => {
-    expect(fizzBuzz(15)).toBe("FizzBuzz")
+    expect(fizzBuzz(135)).toBe("FizzBuzz")
 })
 test('divisible_ni_par_3_ni_par_5', () => {
-    expect(fizzBuzz(1)).toBe(undefined)
+    expect(fizzBuzz(136)).toBe(undefined)
 })
 
