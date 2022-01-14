d7b8bb2169b4b761b658399080862b547431a88e
TFache <thom.fache@orange.fr>
2022-01-14 16:41:31 +0100

    feature: fizzbuzz function

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,7 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+    if(n%3 === 0 && n%5 !== 0) return "Fizz"
+    if(n%5 === 0 && n%3 !== 0) return "Buzz"
+    if(n%3 === 0 && n%5 === 0) return "FizzBuzz"
 }
 
 module.exports = fizzBuzz
