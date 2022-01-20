3bdcb743ad54bc537f33119c9d3bcbb0a6ec440e
TFache <thom.fache@orange.fr>
2022-01-20 21:23:51 +0100

    feat/unit test anniversaire_dans_le_futur

--- test/computeAge.test.js
+++ test/computeAge.test.js
@@ -2,6 +2,12 @@ const computeAge = require('../src/computeAge')
 
 const birthday = new Date(1999,11,19)
 const dateAfterBirthday = new Date(2022,11,29)
+const dateBeforeBirthday = new Date(1979, 1, 12)
+
 test('age_valide', () => {
     expect(computeAge(birthday,dateAfterBirthday)).toBe(23)
+})
+
+test('anniversaire_dans_le_futur', () => {
+    expect(computeAge(birthday,dateBeforeBirthday)).toBe("birthday in the future compared to currentDate.")
 })
