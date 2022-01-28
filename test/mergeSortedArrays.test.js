f592df57c47761c5d019bc241f6871e52229f99a
TFache <thom.fache@orange.fr>
2022-01-28 15:16:00 +0100

    feat/unit test une_entree_non_tableau_renvoie_erreur

--- test/mergeSortedArrays.test.js
+++ test/mergeSortedArrays.test.js
@@ -3,3 +3,7 @@ const mergeSortedArrays = require('../src/mergeSortedArrays')
 test("test_exemple_du_sujet", () => {
     expect(mergeSortedArrays([1, 3, 5], [2, 4])).toStrictEqual([1, 2, 3, 4, 5])
 })
+
+test("une_entree_non_tableau_renvoie_erreur", () => {
+    expect(mergeSortedArrays("test", [2, 4])).toBe("One or both parameter must be array")
+})
