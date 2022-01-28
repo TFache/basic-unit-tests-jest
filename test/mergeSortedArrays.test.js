28888f5aa057c417e2c3660e09b99d691abe3467
TFache <thom.fache@orange.fr>
2022-01-28 15:28:16 +0100

    feat/unit test une_valeur_se_repete

--- test/mergeSortedArrays.test.js
+++ test/mergeSortedArrays.test.js
@@ -7,3 +7,7 @@ test("test_exemple_du_sujet", () => {
 test("une_entree_non_tableau_renvoie_erreur", () => {
     expect(mergeSortedArrays("test", [2, 4])).toBe("One or both parameter must be array")
 })
+
+test("une_valeur_se_repete", () => {
+    expect(mergeSortedArrays([1, 3, 5], [3, 4])).toStrictEqual([1, 3, 3, 4, 5])
+})
